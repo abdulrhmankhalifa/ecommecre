b45bcc23581da53072d615a6cfdc9278b7f37924
Abdulrhman Ahmed <andulrhman16@gmail.com>
2026-09-08 18:09:22 +0300

    finsh build bottom nav bar

--- lib/features/commerce/ui/screens/main_tabs_screen.dart
+++ lib/features/commerce/ui/screens/main_tabs_screen.dart
@@ -1,10 +1,42 @@
+import 'package:ecommerce/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 
-class MainTabsScreen extends StatelessWidget {
+import '../widgets/custom_bottom_navigation_bar.dart';
+import 'tabs/categories_tab.dart';
+import 'tabs/home_tab.dart';
+import 'tabs/profile_tab.dart';
+import 'tabs/wishlist_tab.dart';
+
+class MainTabsScreen extends StatefulWidget {
   const MainTabsScreen({super.key});
 
   @override
+  State<MainTabsScreen> createState() => _MainTabsScreenState();
+}
+
+class _MainTabsScreenState extends State<MainTabsScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _tabs = const [
+    HomeTab(),
+    CategoriesTab(),
+    WishlistTab(),
+    ProfileTab(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: IndexedStack(index: _selectedIndex, children: _tabs),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
